5f930e85d0ef57b3b58dbfdd4dc3eb297bbd608e
IlyasAtabaev731 <pulpasaur731@gmail.com>
2025-04-20 23:45:41 +0300

    change api url again

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_URl = 'http://147.45.109.174:8000/api/api'
+export const API_URl = 'http://147.45.109.174:8000/api/api/'
 // export const API_URl = 'http://localhost:8080/api';
 export const $api = axios.create({
   withCredentials: false,
